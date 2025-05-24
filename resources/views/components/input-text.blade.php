ce2817edfab5083beb215bd80c4ac00988ee2b63
gledysaprill <gledysaprill@gmail.com>
2025-05-24 15:55:59 +0700

    edit: base register page

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,14 +1,11 @@
-@props(['type' => 'text', 'name', 'label', 'placeholder' => ''])
-
 <div class="mb-3 text-start">
     <label for="{{ $name }}" class="form-label font-playfair main-color">
         {{ $label }}
     </label>
     <input 
-        class = "form-control form-theme border-0"
-        type="{{ $type }}" 
-        name="{{ $name }}" 
-        id="{{ $name }}" 
-        placeholder="{{ $placeholder }}" 
-    >
-</div>
+    class="form-control form-theme border-0"
+    type="{{ $type ?? 'text' }}"
+    name="{{ $name }}"
+    id="{{ $name }}"
+    placeholder="{{ $placeholder ?? '' }}"
+    ></div>
