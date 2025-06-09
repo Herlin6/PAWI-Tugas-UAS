95aa0a692b05f6fcafc4f88928fbe075a07de59e
Herlin6 <156332335+Herlin6@users.noreply.github.com>
2025-06-09 21:51:16 +0700

    edit: stats-card partial in books

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Loan;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $totalBooks = Book::count();
+        $totalMembers = User::where('role', 'member')->count();
+        $totalLoans = Loan::count();
         $topBooks = Book::select('title')
             ->withCount('loans')
             ->orderByDesc('loans_count')
@@ -21,7 +26,10 @@ class DashboardController extends Controller
         });
 
         return view('dashboard.index', [
-            'chartData' => $chartData
+            'chartData' => $chartData,
+            'totalBooks' => $totalBooks,
+            'totalMembers' => $totalMembers,
+            'totalLoans' => $totalLoans
         ]);
     }
     
--- resources/views/dashboard/partials/stats-cards.blade.php
+++ resources/views/dashboard/partials/stats-cards.blade.php
@@ -1,14 +1,14 @@
 <div class="row justify-content-center gx-3 gap-lg-4 gap-3">
     <x-card class="col-lg-3 col-sm-10">
         <div>Total Books</div>
-        <h1>1000</h1>
+        <h1>{{ $totalBooks }}</h1>
     </x-card>
     <x-card class="col-lg-3 col-sm-10">
         <div>Total Members</div>
-        <h1>650</h1>
+        <h1>{{ $totalMembers }}</h1>
     </x-card>
     <x-card class="col-lg-3 col-sm-10">
         <div>Total Loans</div>
-        <h1>320</h1>
+        <h1>{{ $totalLoans }}</h1>
     </x-card>
 </div>
